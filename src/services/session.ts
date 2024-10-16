9c85eed25ed8e6f4d7004940ecb33a9ce9e23258
Ian Felix <ianfelix030@gmail.com>
2024-10-16 00:41:31 -0300

    refactor: adjust session

--- src/services/session.ts
+++ src/services/session.ts
@@ -1,7 +1,9 @@
+import { User } from '@/types/session';
 import { cookies } from 'next/headers';
 
 const EMAIL_COOKIE_NAME = 'USER_EMAIL';
 const PASSWORD_COOKIE_NAME = 'USER_PASSWORD';
+const USER_DATA_COOKIE_NAME = 'USER_DATA';
 
 export async function loginUser(credentials: {
   email: string;
@@ -31,15 +33,42 @@ export async function loginUser(credentials: {
   });
 }
 
-export async function getUserEmail() {
-  return cookies().get(EMAIL_COOKIE_NAME)?.value;
-}
-
-export async function setUserEmail(email: string) {
-  cookies().set(EMAIL_COOKIE_NAME, email, { maxAge: 7 * 24 * 60 * 60 }); // 7 days
+export async function getUser(): Promise<User | null> {
+  const user = cookies().get(USER_DATA_COOKIE_NAME)?.value;
+  return user ? JSON.parse(user) : null;
 }
 
 export async function clearUserSession() {
   cookies().delete(EMAIL_COOKIE_NAME);
   cookies().delete(PASSWORD_COOKIE_NAME);
+  cookies().delete(USER_DATA_COOKIE_NAME);
+}
+
+export async function registerUser(userData: {
+  name: string;
+  email: string;
+  password: string;
+  cep: string;
+  street: string;
+  neighborhood: string;
+  number: string;
+  city: string;
+  state: string;
+}): Promise<boolean> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const cookieStore = cookies();
+      cookieStore.set(EMAIL_COOKIE_NAME, userData.email, {
+        maxAge: 7 * 24 * 60 * 60,
+      }); // 7 days
+      cookieStore.set(PASSWORD_COOKIE_NAME, userData.password, {
+        maxAge: 7 * 24 * 60 * 60,
+      }); // 7 days
+      cookieStore.set(USER_DATA_COOKIE_NAME, JSON.stringify(userData), {
+        maxAge: 7 * 24 * 60 * 60,
+      }); // 7 days
+
+      resolve(true);
+    }, 1000);
+  });
 }
---
+++ src/types/session.ts
@@ -0,0 +1,11 @@
+
+export type User = {
+  name: string;
+  email: string;
+  password: string;
+  cep: string;
+  street: string;
+  neighborhood: string;
+  number: string;
+  city: string;
+};
